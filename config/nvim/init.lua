59e9874119ee9f35a7ef513588ef808d86d891cc
Tetsu-is <tetsu.ismt@gmail.com>
2026-03-06 14:19:48 +0900

    temp

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -3,3 +3,6 @@ require("config.lazy")
 -- vim.api.nvim_set_hl(0, "@keyword.import.python", { link = "Keyword" })
 -- vim.api.nvim_set_hl(0, "@module.python", { link = "Identifier" })
 vim.opt.clipboard = "unnamedplus"
+
+local lspconfig = require("lspconfig")
+lspconfig.kotlin_language_server.setup({})
